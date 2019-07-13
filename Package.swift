703558ddda795d1aad8baa9d3d1cceeb5dc31f87
iMoe <iMoeNya@outlook.com>
2019-07-13 11:05:00 +0800

    Update Swift Package Manager Spec

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "ResponsiveButton",
+    platforms: [ .iOS(.v9) ],
+    swiftLanguageVersions: [ .v5 ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
